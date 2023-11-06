e578e036027afe9409e2f8481fbd94f103cea0ea
Nnamdi <nnamdi.nnah@routepayng.com>
2023-11-06 07:45:13 +0100

    add code structure

---
+++ admin/controller/extension/customgateway.php
@@ -0,0 +1,17 @@
+<?php
+class ControllerExtensionPaymentCustomGateway extends Controller {
+    public function index() {
+        // Add code to configure and display settings in the admin panel
+    }
+
+    public function install() {
+        // Add code to create necessary database tables and set default settings
+    }
+
+    public function uninstall() {
+        // Add code to remove database tables and settings
+    }
+
+    // Add more methods for handling payments, order status updates, etc.
+}
+?>
---
+++ catalog/controller/extension/payment/customgateway.php
@@ -0,0 +1,15 @@
+<?php
+
+class ControllerExtensionPaymentCustomGateway extends Controller {
+    public function index() {
+        // Add code to display the payment gateway selection during checkout
+    }
+
+    public function confirm() {
+        // Add code to handle the confirmation of payment
+    }
+
+    // Add more methods for payment processing and order updates
+}
+
+?>
